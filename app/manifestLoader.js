a98e0a423747e9c825fab7b2cbbe5d0c5126186b
mbrechet <mbrechet.ext@orange.com>
2014-07-29 22:20:09 +0200

    correct ManifestParsing
    set new params of xml2json function to remove $ params in manifest
    parsing

--- app/manifestLoader.js
+++ app/manifestLoader.js
@@ -1,10 +1,14 @@
 var ManifestLoader = function(){
 	this.manifest = null;
+	this.baseUrl = "";
 };
 
 ManifestLoader.prototype.load = function(/*url*/ url){
 	var xhr = new XMLHttpRequest();
 	var deferred = Q.defer();
+	this.manifest = null;
+	this.baseUrl = url.substring(0, url.lastIndexOf("/")+1);
+	console.info("baseUrl",this.baseUrl);
 	xhr.open("GET",url,true);
 	xhr.responseType =	"text";
 	xhr.onload = (function(){
@@ -17,11 +21,69 @@ ManifestLoader.prototype.load = function(/*url*/ url){
 
 ManifestLoader.prototype.onHaveManifest = function(data) {
 	
-	var parser =  new DOMParser();
-	var xmlManifest = parser.parseFromString(data,"text/xml",0);
-	this.manifest  = xml2json(xmlManifest)["#document"];	
+	
+	var jsonManifest = xml2json(data,{attrkey:"__proto__",charkey:"_",normalize:false});
+
+	// ici on construit un objet manifest qui nous intéresse  partir des élément reçus
+	
+
+	this.manifest  = this.processManifest(jsonManifest);
+	return this.manifest;
 };
 
 ManifestLoader.prototype.onError = function(e) {
 	// body...
 };
+
+
+ManifestLoader.prototype.processManifest = function(jsonManifest) {
+	debugger;
+	var manifest = {
+		type: jsonManifest.MPD.type,
+		minBufferTime: this.parseDuration(jsonManifest.MPD.minBufferTime),
+		mediaPresentationDuration: this.parseDuration(jsonManifest.MPD.mediaPresentationDuration),
+		baseUrl: jsonManifest.MPD.baseUrl || this.baseUrl,
+	};
+
+	// on va recréer les AdpatationSet video et audio
+	// ce player ne va géré qu'une seule périod
+	var period = Array.isArray(jsonManifest.MPD.Period)? jsonManifest.MPD.Period[0] : jsonManifest.MPD.Period;
+	var adaptationSets = Array.isArray(period.AdaptationSet) ? period.AdaptationSet : [period.AdaptationSet];
+
+	
+	for(var i=0; i<adaptationSets.length;i++){
+		var adaptationSet = adaptationSets[i];
+		if(adaptationSet.mimeType.indexOf("video")!=-1){
+			manifest.videoSet = adaptationSet;
+		}
+		if(adaptationSet.mimeType.indexOf("audio")!=1){
+			manifest.audioSet = adaptationSet;
+		}
+
+		// here comes text
+	}
+
+	return manifest;
+};
+// fonction qui transofrme une durée iso 8601 en seconde
+ManifestLoader.prototype.parseDuration = function(isoDuration) {
+	  // format PYMTHMS
+      var durationRegex = /^P(([\d.]*)Y)?(([\d.]*)M)?(([\d.]*)D)?T?(([\d.]*)H)?(([\d.]*)M)?(([\d.]*)S)?/;
+      var match = durationRegex.exec(isoDuration);
+      console.info("Matching de la durée ::: ",isoDuration,match);
+      // on retrouve les valeur qui nous intéress au position 2,4,6,8,10,12
+       return  (parseFloat(match[2] || 0) * ManifestLoader.SECONDS_IN_YEAR +
+       			parseFloat(match[4] || 0) * ManifestLoader.SECONDS_IN_MONTH+
+       			parseFloat(match[6] || 0) * ManifestLoader.SECONDS_IN_DAY+
+       			parseFloat(match[8] || 0) * ManifestLoader.SECONDS_IN_HOUR+
+       			parseFloat(match[10] || 0) * ManifestLoader.SECONDS_IN_MIN+
+       			parseFloat(match[12] || 0));
+
+};
+
+// constants
+ManifestLoader.SECONDS_IN_YEAR = 365 * 24 * 60 * 60;
+ManifestLoader.SECONDS_IN_MONTH = 30 * 24 * 60 * 60; // not precise!
+ManifestLoader.SECONDS_IN_DAY = 24 * 60 * 60;
+ManifestLoader.SECONDS_IN_HOUR = 60 * 60;
+ManifestLoader.SECONDS_IN_MIN = 60;
