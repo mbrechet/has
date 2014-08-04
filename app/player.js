dc7b33cf1fd4e460a1581db9f0a3fe81460f2580
mbrechet <mbrechet.ext@orange.com>
2014-08-04 12:17:46 +0200

    initialistion mediasource + sourcebuffer

--- app/player.js
+++ app/player.js
@@ -30,18 +30,50 @@ Player.prototype.load = function(url) {
 	var deferred = Q.defer();
 	this.manifestLoader.load(url).then((function(manifest){
 		this.manifestModel = manifest;
+		this.setSource("video",this.manifestModel.videoSet);
+		this.setSource("audio",this.manifestModel.audioSet);
 		deferred.resolve(manifest);
 	}).bind(this));
 	return deferred.promise;
 };
 
-Player.prototype.setSource= function(type, representationSet) {
+
+Player.prototype.play = function(videoQualityId, audioQualityId) {
+	// si les 2 objet existent, on peut créer les différents sourcebuffer
+	if(this.manifestModel && this.mediasource){
+		var videoRepresentation = this.manifestModel.getRepresentation("video",videoQualityId);
+		var audioRepresentation = this.manifestModel.getRepresentation("audio",audioQualityId);
+
+		var videoCodec = this.manifestModel.videoSet.mimeType + ';codecs="'+ videoRepresentation.codecs+'"';
+		var audioCodec = this.manifestModel.audioSet.mimeType + ';codecs="'+ audioRepresentation.codecs+'"';
+		
+		//test du support avant l'initialization
+		if(this.videoModel.canPlayType(videoCodec) === "probably"){
+			this.videoController.initialize(this.mediasource.addSourceBuffer(videoCodec));
+		}else{
+			alert("impossible de lire le codec suivant "+ videoCodec);
+		}
+		if(this.videoModel.canPlayType(audioCodec) === "probably"){
+			this.audioController.initialize(this.mediasource.addSourceBuffer(audioCodec));
+		}else{
+			alert("impossible de lire le codec suivant "+ audioCodec);
+		}
+
+	}
+
+};
+
+Player.prototype.setSource= function(type, adaptationSet) {
+	
 	if(type === "video"){
-		this.videoController = new BufferController(type,representationSet);
+		this.videoController = new BufferController(type,adaptationSet);
+		//this.videoController.setBuffer(this.mediasource.addSourceBuffer(codec));
+
 	}
 
 	if(type === "audio"){
-		this.audioController = new BufferController(type,representationSet);
+		this.audioController = new BufferController(type,adaptationSet);
+		//this.audioController.setBuffer(this.mediasource.addSourceBuffer(codec));
 	}
 
 	// here put text
