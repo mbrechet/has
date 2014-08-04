115c62862346944eb5b6423538757c18db47649a
mbrechet <mbrechet.ext@orange.com>
2014-08-04 10:28:35 +0200

    add manifest load in player

--- app/player.js
+++ app/player.js
@@ -1,7 +1,48 @@
-var Player = function(){
+var Player = function(videoNode){
+	
+	// initialize controllers
+	this.videoController = null;
+	this.audioController = null;
+	this.manifestLoader = null;
 
-}
+	this.videoModel = videoNode;
+	this.manifestModel = null;
+	this.mediasource = this.getMediaSource();
+
+	// ici on attache la mediaSource à la balise video
+	this.videoModel.src = window.URL.createObjectURL(this.mediasource);
+
+};
+
+Player.prototype.getMediaSource = function() {
+
+	var MediaSource = window.MediaSource || window.WebKitMediaSource || null;
+	if(MediaSource){
+ 		return new MediaSource();
+	}else{
+		return null;
+	}
+};
+
 
 Player.prototype.load = function(url) {
-	// body...
+	this.manifestLoader = new ManifestLoader();
+	var deferred = Q.defer();
+	this.manifestLoader.load(url).then((function(manifest){
+		this.manifestModel = manifest;
+		deferred.resolve(manifest);
+	}).bind(this));
+	return deferred.promise;
+};
+
+Player.prototype.setSource= function(type, representationSet) {
+	if(type === "video"){
+		this.videoController = new BufferController(type,representationSet);
+	}
+
+	if(type === "audio"){
+		this.audioController = new BufferController(type,representationSet);
+	}
+
+	// here put text
 };
